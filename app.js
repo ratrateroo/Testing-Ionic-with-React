34471f430d13863e3243c190a74b488dc7f5e744
Lei <ratrateroo@gmail.com>
2021-01-08 16:29:40 +0800

    fixing ionicon cdn

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const calculateBtn = document.querySelector('ion-button');
+const calculateBtn = document.getElementById('calc-btn');
+const resetBtn = document.getElementById('reset-btn');
 const heightInput = document.getElementById('height-input');
 const weightInput = document.getElementById('weight-input');
+
+const resetInputs = () => {
+	heightInput.value = '';
+	weightInput.value = '';
+};
+
 const calculateBmi = () => {
 	const enteredHeight = +heightInput.value;
 	const enteredWeight = +weightInput.value;
 
 	const bmi = enteredWeight / (enteredHeight * enteredHeight);
+
+	console.log(bmi);
 };
 
 calculateBtn.addEventListener('click', calculateBmi);
+resetBtn.addEventListener('click', resetInputs);
